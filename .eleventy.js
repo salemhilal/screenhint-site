f80f60c3e43f76365f1e56ba73bdbc7642cc9163
Salem Hilal <salemhilal@gmail.com>
2021-06-19 23:20:18 -0400

    Ok here we go - optimized image time
    Now to do the ones where I set an image as a div background

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,40 @@ const htmlmin = require("html-minifier");
 const markdownIt = require('markdown-it');
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const mdClass = require("@toycode/markdown-it-class");
+const Image = require("@11ty/eleventy-img");
+
+// https://www.11ty.dev/docs/plugins/image/#use-this-in-your-templates
+async function imageShortcode(src, alt, cls) {
+  let metadata = await Image(src, {
+    widths: [300, 600, null],
+    formats: ["webp", "jpeg"],
+    urlPath: "/img/generated/",
+    outputDir: "./src/static/img/generated/",
+  });
+
+  const sizes = "(min-width: 30em) 50vw, 100vw";
+
+  const imageAttributes = {
+    class: cls,
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: "inline"
+  });
+}
 
 module.exports = function (eleventyConfig) {
   // PLUGINS
   eleventyConfig.addPlugin(pluginRss);
+
+  // Image shortcodes
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
   // shortcode to render markdown from string => {{ STRING | markdown | safe }}
   eleventyConfig.addFilter('markdown', function(value) {
--- src/credits.md
+++ src/credits.md
@@ -8,4 +8,4 @@ I wrote ScreenHint because it helps me think while I work. I don't have the best
 
 ScreenHint is heavily inspired by [Snappy](http://snappy-app.com/), a snapshotting app. Snappy no longer appears to be maintained, and it seems to have broken in macOS 11. My hope for ScreenHint is not to be a re-implementation of Snappy, but a re-imagining of what a tool like Snappy could be like with a focus on productivity. If you maintain Snappy, know that I loved your app and would love to [talk more about it with you](mailto:hi@salem.io).
 
-This landing site is a fork of [11ty-landing-page](https://github.com/ttntm/11ty-landing-page) by [Tom Doe](https://ttntm.me/). The site is powered by [11ty](11ty.dev), which I absolutely love using.
+This landing site is a fork of [11ty-landing-page](https://github.com/ttntm/11ty-landing-page) by [Tom Doe](https://ttntm.me/). The site is powered by [11ty](11ty.dev), which I absolutely love using. I designed the logo and icons in Figma with the help of [this Figma template](https://www.figma.com/community/file/857303226040719059/macOS-Big-Sur-Icon-Template) by [Václav Vančura](https://vancura.design/).
